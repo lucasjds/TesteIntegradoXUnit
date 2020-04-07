1eedca2a4862ed3b84447f033320ce3fdc59ae88
Lucas Souza <lucassouza690@gmail.com>
2020-04-07 17:52:01 -0300

    Aula 6

--- src/Alura.CoisasAFazer.WebApp/Controllers/TarefasController.cs
+++ src/Alura.CoisasAFazer.WebApp/Controllers/TarefasController.cs
@@ -2,27 +2,38 @@
 using Alura.CoisasAFazer.WebApp.Models;
 using Alura.CoisasAFazer.Core.Commands;
 using Alura.CoisasAFazer.Services.Handlers;
+using Microsoft.Extensions.Logging;
+using Alura.CoisasAFazer.Infrastructure;
 
 namespace Alura.CoisasAFazer.WebApp.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TarefasController : ControllerBase
-    {
-        [HttpPost]
-        public IActionResult EndpointCadastraTarefa(CadastraTarefaVM model)
-        {
-            var cmdObtemCateg = new ObtemCategoriaPorId(model.IdCategoria);
-            var categoria = new ObtemCategoriaPorIdHandler().Execute(cmdObtemCateg);
-            if (categoria == null)
-            {
-                return NotFound("Categoria não encontrada");
-            }
+  [Route("api/[controller]")]
+  [ApiController]
+  public class TarefasController : ControllerBase
+  {
+    IRepositorioTarefas _repo;
+    ILogger<CadastraTarefaHandler> _logger;
 
-            var comando = new CadastraTarefa(model.Titulo, categoria, model.Prazo);
-            var handler = new CadastraTarefaHandler();
-            handler.Execute(comando);
-            return Ok();
-        }
+    public TarefasController(IRepositorioTarefas repo, ILogger<CadastraTarefaHandler> logger)
+    {
+      _repo = repo;
+      _logger = logger;
     }
+
+    [HttpPost]
+    public IActionResult EndpointCadastraTarefa(CadastraTarefaVM model)
+    {
+      var cmdObtemCateg = new ObtemCategoriaPorId(model.IdCategoria);
+      var categoria = new ObtemCategoriaPorIdHandler(_repo).Execute(cmdObtemCateg);
+      if (categoria == null)
+      {
+        return NotFound("Categoria não encontrada");
+      }
+
+      var comando = new CadastraTarefa(model.Titulo, categoria, model.Prazo);
+      var handler = new CadastraTarefaHandler(_repo, _logger);
+      handler.Execute(comando);
+      return Ok();
+    }
+  }
 }
--- src/Alura.CoisasAFazer.WebApp/Startup.cs
+++ src/Alura.CoisasAFazer.WebApp/Startup.cs
@@ -34,7 +34,7 @@ namespace Alura.CoisasAFazer.WebApp
 
             services.AddDbContext<DbTarefasContext>(cfg => cfg.UseSqlServer(Configuration.GetConnectionString("DbTarefas")));
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
